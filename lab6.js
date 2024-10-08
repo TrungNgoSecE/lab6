06aa23aa5c572f58f738e1479a8d124cf50ac3b7
TrungNgoSecE <trungngotien2k4@gmail.com>
2024-10-08 09:02:04 -0400

    Task 3 Discovering Functions

--- lab6.js
+++ lab6.js
@@ -11,3 +11,15 @@ for (let i = 0; i <= 10; i++){
 for (let i = 0; i <= 4; i++){
   console.log(i*3);
 }
+
+//task 3
+function addNumbers(num1, num2){
+  return num1 + num2;
+}
+console.log(addNumbers(5, 6))
+
+function multiplyNumbers(x, y){
+  let result = x * y;
+  return result;
+}
+console.log(multiplyNumbers(5,6))
