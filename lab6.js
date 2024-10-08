5b925bdff5b300f946360c0d15a62fd14c4784ec
TrungNgoSecE <trungngotien2k4@gmail.com>
2024-10-08 08:57:59 -0400

    Task 2 Exploring Loops

--- lab6.js
+++ lab6.js
@@ -4,3 +4,10 @@ let myString = `Hello, JavaScript!`
 console.log(myNumber, myString)
 
 //task 2
+for (let i = 0; i <= 10; i++){
+  console.log(i);
+}
+
+for (let i = 0; i <= 4; i++){
+  console.log(i*3);
+}
